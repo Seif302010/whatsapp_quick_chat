80f6c12daf588e72885879b72e92015a6e0cf1b6
Seif302010 <seif302010@gmail.com>
2024-06-11 02:22:50 +0300

    build scribts updated

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
-windres addIcon.rc -O coff -o addIcon-x64.res
-windres addIcon.rc -O coff -F pe-i386 -o addIcon-x32.res
-g++ -static -o whatsapp_quick_chat-x64 main.cpp addIcon-x64.res
-g++ -static -m32 -o whatsapp_quick_chat-x32 main.cpp addIcon-x32.res
+echo off
+set MINGW32=C:\MinGW-W64\mingw32\bin
+set MINGW64=C:\MinGW-W64\mingw64\bin
+echo on
+
+%MINGW64%\windres addIcon.rc -O coff -o addIcon-x64.res
+%MINGW32%\windres addIcon.rc -O coff -F pe-i386 -o addIcon-x32.res
+%MINGW64%\g++ -static -o whatsapp_quick_chat-x64 main.cpp addIcon-x64.res
+%MINGW32%\g++ -static -m32 -o whatsapp_quick_chat-x32 main.cpp addIcon-x32.res
